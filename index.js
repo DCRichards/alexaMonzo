660ad9e243c65618a8a54a4430b9d0c891967130
DCRichards <DCRichards@live.co.uk>
2016-11-06 20:37:39 +0000

    Use arrow functions and const let in index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var Alexa = require('alexa-sdk');
-var log = require('npmlog');
-var Monzo = require('./Monzo');
+const Alexa = require('alexa-sdk');
+const Monzo = require('./Monzo');
 
-exports.handler = function(event, context, callback) {
-   var alexa = Alexa.handler(event, context);
-   var monzo = new Monzo(event.session.user.accessToken);
+exports.handler = (event, context, callback) => {
+   let alexa = Alexa.handler(event, context);
+   let monzo = new Monzo(event.session.user.accessToken);
    alexa.resources = require('./languages');
    alexa.registerHandlers(require('./handlers')(monzo));
    alexa.execute();
 };
+
